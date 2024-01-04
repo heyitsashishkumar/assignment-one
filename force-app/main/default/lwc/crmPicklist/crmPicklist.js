608790a1abdc342a66160c9eb904631b53160a7f
Ashish Kumar <heyitsashishkumar@gmail.com>
2024-01-04 16:04:24 +0530

    added absolute picklist

--- force-app/main/default/lwc/crmPicklist/crmPicklist.js
+++ force-app/main/default/lwc/crmPicklist/crmPicklist.js
@@ -1,3 +1,26 @@
 import { LightningElement } from 'lwc';
+import search from '@salesforce/apex/CRMSearch.search';
 
-export default class CrmPicklist extends LightningElement {}
+export default class CrmPicklist extends LightningElement {
+    searchResults;
+
+    handleChange(event) {
+        let searchInput = event.target.value;
+
+        if (searchInput) {
+            // Check if searchInput is larger than 2 characters
+            if (searchInput.length > 2) {
+                // Call Apex method imperatively
+                this.handleSearch(searchInput);
+            }
+        }
+    }
+
+    async handleSearch(searchInput) {
+        try {
+            this.searchResults = JSON.parse(await search({ searchKey: searchInput }));
+        } catch (error) {
+            console.log('Error: ' + error);
+        }
+    }
+}
