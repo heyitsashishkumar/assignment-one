9b7944f978f3909f8ded48722477ef4b74968d0c
Ashish Kumar <heyitsashishkumar@gmail.com>
2024-01-21 13:26:40 +0530

    Till session 4

--- force-app/main/default/lwc/crmPicklist/crmPicklist.js
+++ force-app/main/default/lwc/crmPicklist/crmPicklist.js
@@ -2,10 +2,12 @@ import { LightningElement } from 'lwc';
 import search from '@salesforce/apex/CRMSearch.search';
 
 export default class CrmPicklist extends LightningElement {
-    searchResults;
+    searchResults = [];
+    showDropdown = false;
 
     handleChange(event) {
         let searchInput = event.target.value;
+        console.log('Search Input: ' + searchInput);
 
         if (searchInput) {
             // Check if searchInput is larger than 2 characters
@@ -16,9 +18,38 @@ export default class CrmPicklist extends LightningElement {
         }
     }
 
+    handleSelect(event) {
+        console.log('Selected Record: ' + JSON.stringify(event.currentTarget.dataset));
+    }
+
     async handleSearch(searchInput) {
         try {
-            this.searchResults = JSON.parse(await search({ searchKey: searchInput }));
+            let records = (JSON.parse(await search({ searchKey: searchInput }))).flat();
+
+            let searchedRecords = [];
+            records.forEach(record => {
+                console.log('Record: ' + record);
+                let searchedRecord = {};
+                searchedRecord["name"] = record.Name;
+                searchedRecord["id"] = record.Id;
+                searchedRecord["type"] = record.attributes.type;
+                searchedRecord["icon"] = `standard:${(record.attributes.type).toLowerCase()}`;
+
+                if (record.attributes.type === "Contact") {
+                    searchedRecord["details"] = record.Email;
+                } else if (record.attributes.type === "Account") {
+                    searchedRecord["details"] = record.Website;
+                } else if (record.attributes.type === "Lead") {
+                    searchedRecord["details"] = record.Email;
+                }
+
+                console.log('Searched Record: ' + JSON.stringify(searchedRecord));
+
+                searchedRecords.push(searchedRecord);
+            });
+
+            this.searchResults = searchedRecords;
+            this.showDropdown = true;
         } catch (error) {
             console.log('Error: ' + error);
         }
